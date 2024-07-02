e7a82520a6baf6a9177b4da7ab047c62e915058c
emimgg <ultralightened@gmail.com>
2024-07-02 19:28:05 -0400

    Add functionality
    Made Book Constructor, functions to push book to
    array and display them on the DOM

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
 const bookArray = [];
 
 const container = document.querySelector(".books");
+const addBookBtn = document.querySelector("#add-new");
+const dialog = document.querySelector("dialog");
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
+const bookIsRead = document.querySelector("#is-read");
+
+const bookForm = document.querySelector("form");
+
+bookForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pages = bookPages.value;
+    let isRead = bookIsRead.value;
+
+    let book = new Book(title, author, pages, isRead);
+    book.addBookToArray();
+    displayBooks();
+    dialog.close();
+    clearInput();
+});
+
+function clearInput() {
+    bookTitle.value= "";
+    bookAuthor.value= "";
+    bookPages.value= "";
+    bookIsRead.value= "";
+}
+
+addBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+})
 
 
-function Book(title, author, pages, isread) {
+function Book(title, author, pages, isRead) {
     this.title = title;
-    this.author - author;
+    this.author = author;
     this.pages = pages;
-    this.isread = isread;
+    this.isRead = isRead;
 }
 
 Book.prototype.addBookToArray = function() {
     bookArray.push(this)
 }
 
-
-const book1 = new Book("Lord of the Rings: The fellowship of The Ring", "JRR Tolkien", "536", "not yet")
-
-book1.addBookToArray();
-console.log(bookArray);
-displayBook();
-
-function displayBook() {
-    for (let i of bookArray) {
+function displayBooks(book) {
+    container.innerHTML = "";
+    for (let book of bookArray) {
         const bookCard = document.createElement("article");
         bookCard.classList.add("card");
-        bookCard.textContent = i.title;
+        bookCard.textContent = `${book.title} by ${book.author}`;
         container.appendChild(bookCard);
     }
 }
+
+
+// const book1 = new Book("Lord of the Rings: The fellowship of The Ring", "JRR Tolkien", "536", "not yet")
+
+// book1.addBookToArray();
+// console.log(bookArray);
+// displayBook();
